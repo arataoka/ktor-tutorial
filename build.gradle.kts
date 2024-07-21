daa58e63a62e62ffdd4e1cf38f54e140795bf4b0
arataoka <arata.okayasu@gmail.com>
2024-07-21 18:30:55 +0900

    ktor tutorial

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+
+val kotlin_version: String by project
+val ktor_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.0.0"
+    id("io.ktor.plugin") version "2.3.12"
+}
+
+group = "com.example"
+version = "0.0.1"
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // The new dependency to be added
+    implementation("io.ktor:ktor-server-status-pages:$ktor_version")
+    // The existing dependencies
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-config-yaml")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
